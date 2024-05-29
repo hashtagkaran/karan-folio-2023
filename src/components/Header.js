bf7105a81d9c2c9909a5c0f905f360e13927dfbf
Karan Singh <hashtagkaran@Karans-MacBook-Air.local>
2024-05-29 15:13:31 +0530

    updated resume file

--- src/components/Header.js
+++ src/components/Header.js
@@ -63,13 +63,13 @@ const Header = () => {
                   </a>
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link to="/about">
                   <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                     About
                   </a>
                 </Link>
-              </li>
+              </li> */}
 
               <li>
                 <Link to="/contact">
